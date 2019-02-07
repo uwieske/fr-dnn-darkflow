3176e735d87fc6c326e97644931634654d422780
uwieske <uwieske@gmail.com>
2019-02-07 14:36:53 +0100

    flow traincommand script setup

--- train.sh
+++ train.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 CONFIG_DIR=''
-MODEL_CONFIG_FILE=''
-DATASET_DIR=''
-ANNOTATION_DIR=''
-LABELS_FILE=''
-THRESHOLD=0.6
-WEIGHTS_FILE=''
-BATCH_SIZE=
-EPOCH_COUNT=
+MODEL_CONFIG_FILE='/Users/uwieske/ukrec-res/yolo_ukrec.cfg'
+DATASET_DIR='/Users/uwieske/ukrec-res/ukrec-images/images_train/images/'
+ANNOTATION_DIR='/Users/uwieske/ukrec-res/ukrec-images/images_train/annotations/'
+LABELS_FILE='/Users/uwieske/ukrec-res/labels.txt'
+THRESHOLD=0.1
+LOAD_WEIGHTS_FILE='/Users/uwieske/darkflow-res/darkflow/yolo.weights'
+BATCH_SIZE=2
+EPOCH_COUNT=8
+SUMMARY_FOLDER='/Users/uwieske/ukrec-res/summary/'
 
 # Train command
-flow --train --config $CONFIG --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --json
+#--config $CONFIG_DIR
+flow --train --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --summary $SUMMARY_FOLDER  --json
 

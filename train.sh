dbcf018022201ccf3cb8970cdb7c92851c3865c4
uwieske <uwieske@gmail.com>
2019-02-07 13:51:02 +0100

    flow traincommand script setup

--- train.sh
+++ train.sh
@@ -11,7 +11,5 @@ BATCH_SIZE=
 EPOCH_COUNT=
 
 # Train command
-flow --train --config $CONFIG --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR \\
-     --labels $LABELS_FILE --threshold $THRESHOLD --load $WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT \\
-     --json
+flow --train --config $CONFIG --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --json
 

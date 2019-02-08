81487680cd0e5a8fe23e98ed48986307325651c2
uwieske <uwieske@gmail.com>
2019-02-08 16:05:34 +0100

    setting up floydhub for project thru scripts

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-export CONFIG_DIR=''
-export MODEL_CONFIG_FILE='./yolo_ukrec.cfg'
-export DATASET_DIR='/floyd/input/ukrec-images/images_train/images/'
-export ANNOTATION_DIR='/floyd/input/ukrec-images/images_train/annotations/'
-export LABELS_FILE='./labels.txt'
-export THRESHOLD=0.1
-export LOAD_WEIGHTS_FILE='/floyd/input/soccer_ballgame/yolo.weights'
-export BATCH_SIZE=8
-export EPOCH_COUNT=2
-export SUMMARY_FOLDER='summary/'
+CONFIG_DIR=''
+MODEL_CONFIG_FILE='./yolo_ukrec.cfg'
+DATASET_DIR='/floyd/input/ukrec-images/images_train/images/'
+ANNOTATION_DIR='/floyd/input/ukrec-images/images_train/annotations/'
+LABELS_FILE='./labels.txt'
+THRESHOLD=0.1
+LOAD_WEIGHTS_FILE='/floyd/input/soccer_ballgame/yolo.weights'
+BATCH_SIZE=8
+EPOCH_COUNT=2
+SUMMARY_FOLDER='summary/'
 
 # Train command
 #--config $CONFIG_DIR
-sh -c "flow --train --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $LOAD_WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --summary $SUMMARY_FOLDER  --json --savepb"
+flow --train --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $LOAD_WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --summary $SUMMARY_FOLDER  --json --savepb
 

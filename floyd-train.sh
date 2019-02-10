ac9ab429c3ff0be62babb6e91540dcbdc5438686
uwieske <uwieske@gmail.com>
2019-02-11 00:00:02 +0100

    changes which will make first training process work well.

--- floyd-train.sh
+++ floyd-train.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-CONFIG_DIR=''
+#CONFIG_DIR=''
 MODEL_CONFIG_FILE='../yolo_ukrec.cfg'
 DATASET_DIR='/floyd/input/ukrec-images/images_train/images/'
 ANNOTATION_DIR='/floyd/input/ukrec-images/images_train/annotations/'
 LABELS_FILE='../labels.txt'
-THRESHOLD=0.1
+#THRESHOLD=0.1
 LOAD_WEIGHTS_FILE='/floyd/input/soccer_ballgame/yolo.weights'
-BATCH_SIZE=4
-EPOCH_COUNT=1
+BATCH_SIZE=8
+#EPOCH_COUNT=1
 SUMMARY_FOLDER='summary/'
 
 # Train command
 #--config $CONFIG_DIR
 cd /floyd/home/darkflow
-flow --train --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --threshold $THRESHOLD --load $LOAD_WEIGHTS_FILE --batch $BATCH_SIZE --epoch $EPOCH_COUNT --summary $SUMMARY_FOLDER  --json --savepb
+flow --train --trainer adam --batch $BATCH_SIZE --gpu 1.0 --model $MODEL_CONFIG_FILE --dataset $DATASET_DIR --annotation $ANNOTATION_DIR --labels $LABELS_FILE --load $LOAD_WEIGHTS_FILE --summary $SUMMARY_FOLDER --json  --savepb
 

406b0c51a7b623bff748aee57e16ed8a35286d28
uwieske <uwieske@gmail.com>
2019-04-24 13:34:38 +0200

    cleaning, refactoring and reorganizing project

--- floyd-login.sh
+++ floyd-login.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-
+# This shell script will perform a Floydhub login with your API key.
+# The key should not be exposed in this script. Floydhub has a mechanism to make the API key available through
+# an environment variable.
 floyd login -k $MY_FLOYD_API_KEY
--- floyd-prepare.sh
+++ floyd-prepare.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# This shell script accepts only one argument which is your Floydhub project name namespaced by your Floydhub username.
+# Example: "jdoe/my-first-project"
+# It assumed that your script is run in the root of the project so that initialization is done at the toplevel (root) of
+# this project directory.
 FLOYD_PROJECT=$1
 floyd init $FLOYD_PROJECT
 
